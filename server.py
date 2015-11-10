0678bb52c5233accfa6e23050295b9d0752b8c99
Colin McKenna <colinjmckenna@gmail.com>
2015-11-09 16:28:12 -0800

    validate email

--- server.py
+++ server.py
@@ -1,11 +1,21 @@
 from flask import Flask, flash, request, render_template, redirect, session
 from mysqlconnection import MySQLConnector
+import re
 from time import gmtime, strftime
+EMAIL_REGEX = re.compile(r'^[a-za-z0-9\.\+_-]+@[a-za-z0-9\._-]+\.[a-za-z]*$')
 app = Flask(__name__)
 app.secret_key="emailtime"
 mysql = MySQLConnector('emaildb')
 @app.route('/')
 def index():
   emails = mysql.fetch('SELECT * FROM emails')
-  print emails
-  render_template('index.html')
+  return render_template('index.html')
+@app.route('/validate', methods=['POST'])
+def validate():
+  if not EMAIL_REGEX.match(request.form['email']):
+    flash('Please Enter a valid email address.')
+    return redirect('/')
+  else:
+    return render_template('success.html')
+app.run(debug=True)
+

b3b7b1ceec79eecc01f3dc1f1407387cd273c3e3
Colin McKenna <colinjmckenna@gmail.com>
2015-11-09 17:20:35 -0800

    add db

--- server.py
+++ server.py
@@ -6,9 +6,9 @@ EMAIL_REGEX = re.compile(r'^[a-za-z0-9\.\+_-]+@[a-za-z0-9\._-]+\.[a-za-z]*$')
 app = Flask(__name__)
 app.secret_key="emailtime"
 mysql = MySQLConnector('emaildb')
+emails = mysql.fetch('SELECT * FROM emails')
 @app.route('/')
 def index():
-  emails = mysql.fetch('SELECT * FROM emails')
   return render_template('index.html')
 @app.route('/validate', methods=['POST'])
 def validate():
@@ -16,6 +16,8 @@ def validate():
     flash('Please Enter a valid email address.')
     return redirect('/')
   else:
-    return render_template('success.html')
+    insert = "INSERT INTO emails (email, created_at, updated_at) VALUES ('{}', NOW(), NOW())".format(request.form['email'])
+    mysql.run_mysql_query(insert)
+    return render_template('success.html', emails = emails)
 app.run(debug=True)
 
